c0ee47cc6d9fd1eb4092034f2579db57e076099c
djshaioh <dmsqlder@gmail.com>
2024-06-07 18:27:49 +0900

    main.py 변경

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import datetime
+import uvicorn
 
 app = FastAPI()
 
-origins = ["http://127.0.0.1:5500", "http://3.224.62.21"]
+origins = ["http://127.0.0.1:5500", "http://3.224.62.21:8200"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,10 +16,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Message(BaseModel):
+    name: str
+    content: str
+    timestamp: datetime = None  # timestamp 필드를 선택적으로 만듦
+
+guestbook = []
+
+@app.post("/guestbook/")
+async def add_message(message: Message) -> dict:
+    # 현재 시간 기록
+    timestamp = datetime.now()
+    # 새로운 항목 추가
+    new_message = {"name": message.name, "content": message.content, "timestamp": timestamp}
+    guestbook.append(new_message)
+    return new_message  # 새로운 항목 반환
+
+@app.get("/guestbook/")
+async def get_guestbook() -> list:
+    return guestbook
+
 @app.get("/")
 async def welcome() -> dict:
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
